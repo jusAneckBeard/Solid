30a47972a595f63ee1aa3b9ec8283c98a7e176d4
jusAneckBeard <justanrandom@hotmail.com>
2024-12-14 16:10:39 -0600

    Create WrmUp.sol

---
+++ WrmUp.sol
@@ -0,0 +1,12 @@
+//Im working on blockchain. Feel like it would be handy
+//start wit a simple morguge tracker then ima add features
+
+pragma soldity >= 0.7.0 < 0.9.0
+
+contract Deed{
+
+
+
+}
+
+
